17f9f449df25d6d79229a09ca9c20a2f64fde701
K. D. Sunera Avinash Chandrasiri <kdsuneraavinash@gmail.com>
2021-10-05 14:49:51 +0530

    Add affine-qp metric

--- ch_bin/core/clustering/hull_distance.py
+++ ch_bin/core/clustering/hull_distance.py
@@ -35,6 +35,37 @@ def convex_hull_distance(query: np.ndarray, points: np.ndarray, solver: str = "q
     return np.linalg.norm(proj - x)
 
 
+def affine_hull_distance_qp(query: np.ndarray, points: np.ndarray, solver: str = "quadprog") -> float:
+    """
+    Finds distance to the affine hull using the given solver.
+
+    :param query: Point to find the distance from.
+    :param points: Points forming the target convex hull.
+    :param solver: Solver to use. (quadprog/cvxopt)
+    :return: The convex hull distance from the query point to the convex hull formed by the points given.
+    """
+
+    n = len(points)
+
+    mat_a = np.ones((1, n))  # first equality constraint
+    vec_b = np.ones(1)  # first constraint RHS having one
+
+    mat_g = np.zeros(shape=(0, n))  # no second constraint
+    vec_h = np.zeros(0)  # second constraint RHS having 0
+
+    mat_x = points.T  # points in cluster
+    mat_x_t = points
+    x = query.T  # query point
+    x_t = query
+
+    mat_p = 2 * np.matmul(mat_x_t, mat_x)
+    vec_q = (-2 * np.matmul(x_t, mat_x)).T
+
+    alpha = solve_qp(mat_p, vec_q, mat_g, vec_h, mat_a, vec_b, solver)
+    proj = np.matmul(alpha, mat_x_t)
+    return np.linalg.norm(proj - x)
+
+
 def affine_hull_distance(query: np.ndarray, points: np.ndarray) -> float:
     """
     Finds distance to the affine hull using the given solver.
@@ -72,4 +103,6 @@ def calculate_distance(x: np.ndarray, mat_p: np.ndarray, qp_solver: str, metric:
         return convex_hull_distance(x, mat_p, solver=qp_solver)
     if metric == "affine":
         return affine_hull_distance(x, mat_p)
+    if metric == "affine-qp":
+        return affine_hull_distance_qp(x, mat_p, solver=qp_solver)
     raise NotImplementedError(f"Metric {metric} not implemented")
